417e8c0b6d5a7899d0e03e6b418bb8a5e9d48e1c
Ronaldo Rodrigues <ronaldo182019@gmail.com>
2022-07-30 16:44:48 -0300

    desestrutura r

---
+++ util.py
@@ -0,0 +1,17 @@
+def desestrutura_r(binario):
+    """6 bits - opcode | 5 bits registrador | 5 bits registrador | 5 bits registrador | 5 bits shitf amount | 6 bits
+    function """
+    instrucao = {
+        'opcode': binario[0:6],
+        'rd': binario[6:11],
+        'rs1': binario[11:16],
+        'rs2': binario[16:21],
+        'sa': binario[21:26],
+        'fn': binario[26:32]
+    }
+    return instrucao
+
+def hex2bin(hexa):
+    return f"{int(hexa, 16):32b}"
+
+print(hex2bin('0x24020004'))
