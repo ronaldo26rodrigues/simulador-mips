d99a5e5b8f651bf4e6f15040292010b6ec5b9a2d
Vicente Alencar Sampaio <vicentegalencar@gmail.com>
2022-08-06 18:44:05 -0300

    Instruções de 1 a 13(incompleto)

--- mips.py
+++ mips.py
@@ -48,6 +48,102 @@ def add(rd, rs1, rs2):
     registradores[rd] = registradores[rs1] + registradores[rs2]
     return -1
 
+
+def addi(rs, rt, imd):
+    rs = util.bin2dec(rs)
+    rt = util.bin2dec(rt)
+    imd = util.bin2dec(imd)
+    registradores[rs] = registradores[rt] + registradores[imd]
+    return -1
+
+
+def addiu(rs, rt, imd):
+    rs = util.bin2dec(rs)
+    rt = util.bin2dec(rt)
+    imd = util.bin2dec(imd)
+    registradores[rs] = registradores[rt] + registradores[imd]
+    return -1
+
+
+def addu(rd, rs1, rs2):
+    rd = util.bin2dec(rd)
+    rs1 = util.bin2dec(rs1)
+    rs2 = util.bin2dec(rs2)
+    registradores[rd] = registradores[rs1] + registradores[rs2]
+    return -1
+
+
+def andd(rd, rs1, rs2):
+    rd = util.bin2dec(rd)
+    rs1 = util.bin2dec(rs1)
+    rs2 = util.bin2dec(rs2)
+    registradores[rd] = registradores[rs1] and registradores[rs2]
+    return -1
+
+
+def andi(rs, rt, imd):
+    rs = util.bin2dec(rs)
+    rt = util.bin2dec(rt)
+    imd = util.bin2dec(imd)
+    registradores[rs] = registradores[rt] and registradores[imd]
+    return -1
+
+
+def bgtz(rs, rt, imd):
+    rs = util.bin2dec(rs)
+    rt = 0
+    imd = util.bin2dec(imd)
+    if(registradores[rs] > rt): registradores['pc'] + registradores[imd]
+    return -1
+
+
+def beq(rs, rt, imd):
+    rs = util.bin2dec(rs)
+    rt = 0
+    imd = util.bin2dec(imd)
+    if(registradores[rs] > rt): registradores['pc'] + registradores[imd]
+    return -1
+
+
+def bltz(rs, rt, imd):
+    rs = util.bin2dec(rs)
+    rt = 0
+    imd = util.bin2dec(imd)
+    if(registradores[rs] > rt): registradores['pc'] + registradores[imd]
+    return -1
+
+
+def blez(rs, rt, imd):
+    rs = util.bin2dec(rs)
+    rt = 0
+    imd = util.bin2dec(imd)
+    if registradores[rs] > rt: registradores['pc'] + registradores[imd]
+    return -1
+
+
+def bne(rs, rt, imd):
+    rs = util.bin2dec(rs)
+    rt = 0
+    imd = util.bin2dec(imd)
+    if registradores[rs] > rt: registradores['pc'] + registradores[imd]
+    return -1
+
+
+def div(rs1, rs2):
+    rs1 = util.bin2dec(rs1)
+    rs2 = util.bin2dec(rs2)
+    registradores['hi'] = registradores[rs1] % registradores[rs2]
+    registradores['lo'] = registradores[rs1] // registradores[rs2]
+    return -1
+
+
+def divu(rs1, rs2):
+    rs1 = util.bin2dec(rs1)
+    rs2 = util.bin2dec(rs2)
+    registradores['hi'] = registradores[rs1] % registradores[rs2]
+    registradores['lo'] = registradores[rs1] // registradores[rs2]
+    return -1
+
 def sub(rd, rs1, rs2):
     rd = util.bin2dec(rd)
     rs1 = util.bin2dec(rs1)
@@ -102,6 +198,18 @@ opcode = {
 
 functions = {
     '100000': add,
+    '001000': addi,
+    '001001': addiu,
+    '100001': addu,
+    '100100': andd,
+    '001100': andi,
+    '000111': bgtz,
+    '000100': beq,
+    '000001': bltz,
+    '000110': blez,
+    '000101': bne,
+    '011010': div,
+    '011011': divu,
     '001000': jr,
     '100010': sub,
     '000000': sll,
